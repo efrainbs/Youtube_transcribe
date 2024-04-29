7cfe85c42faa40e8c10bd611352a40fccb140aee
ebedoya <efrain.bedoya.s@gmail.com>
2024-04-29 18:39:36 -0500

    update

--- wpr.py
+++ wpr.py
@@ -3,7 +3,7 @@ import whisper
 # Función que transcribe a partir del archivo de audio mp3
 def tran(audio_file, nivel):
     model = whisper.load_model(nivel)
-    result = model.transcribe(audio_file)
+    result = model.transcribe(audio_file, language = "es", fp16 = False)
 
     name_text = audio_file.replace(".mp3", "")
     
