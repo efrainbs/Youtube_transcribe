00b43c42c5438d7f238dc69a4e975ba22beda635
Schwartz <efrain.bedoya.s@gmail.com>
2024-08-08 17:51:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from video_convertidor import video_convertidor
 from pytube import YouTube
 from wpr import tran
+import subprocess
+import whisper
 
 
 file_name = input("ponle un nombre al archivo: ") # Elegir el nombre que tendrá el archivo de salida
@@ -17,16 +18,32 @@ def Download(link):
     print("Hubo un error al descargar el video del URL proporcionado...")
   print("¡Descarga completada con éxito!")
   
-
 # Descarga el video
 Download(link)
+
+# Función que convierte el video mp4 en audio mp3
+def video_convertidor(input_file, outpu_file):
+    ffmpeg_cmd = ["ffmpeg", "-i", input_file, outpu_file]
+
+    try:
+        subprocess.run(ffmpeg_cmd, check = True)
+        print("Convertido con éxito")
+    except subprocess.CalledProcessError as e:
+        print("La conversión falló")
 
 # Convierte el video a audio
 video_convertidor(f"{file_name}.mp4", f"{file_name}.mp3")
 
+# Función que transcribe a partir del archivo de audio mp3
+def tran(audio_file, nivel):
+    model = whisper.load_model(nivel)
+    result = model.transcribe(audio_file, language = "es", fp16 = False)
+
+    name_text = audio_file.replace(".mp3", "")
+    
+    with open(f"{name_text}.txt", "w") as f:
+        f.write(result["text"])
+
 # Convierte el audio a texto
 tran(f"{file_name}.mp3", nivel_modelo_whisper)
-        
-       
-       
 
